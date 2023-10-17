e76d042d32f86e2470427d44a8901a745c10065a
Grzegorz Kędziora <geloke59@gmail.com>
2023-10-17 15:27:33 +0200

    add rediucer

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -1,13 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   list: [],
 };
 
 export const appSlice = createSlice({
-  name: "app",
+  name: 'app',
   initialState,
-  reducers: {},
+  reducers: {
+    addPhotos: (state, action) => {
+      state.photos.push(action.payload);
+    },
+    removePhotos: (state, action) => {
+      state.photos = state.photos.filter(
+        (photo) => photo.id !== action.payload
+      );
+    },
+  },
 });
+
+export const { addPhoto, removePhoto } = appSlice.actions;
 
 export default appSlice.reducer;
